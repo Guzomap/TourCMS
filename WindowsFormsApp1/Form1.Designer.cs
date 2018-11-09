367f0b521587e57f6236aa79e266fc5f8aad0d74
M1F <it-technologies@yandex.ru>
2018-11-10 00:45:14 +0700

    Init upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonDel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.ButtonLink = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(938, 12);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(122, 37);
+            this.OpenButton.TabIndex = 0;
+            this.OpenButton.Text = "Выбрать папку";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(910, 399);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(938, 304);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(1038, 114);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(22, 19);
+            this.buttonAdd.TabIndex = 4;
+            this.buttonAdd.Text = "+";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(938, 114);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(96, 160);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // buttonDel
+            // 
+            this.buttonDel.Location = new System.Drawing.Point(1038, 146);
+            this.buttonDel.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonDel.Name = "buttonDel";
+            this.buttonDel.Size = new System.Drawing.Size(22, 19);
+            this.buttonDel.TabIndex = 6;
+            this.buttonDel.Text = "-";
+            this.buttonDel.UseVisualStyleBackColor = true;
+            this.buttonDel.Click += new System.EventHandler(this.buttonDel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(936, 289);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Ссылка";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(938, 66);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(122, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // ButtonLink
+            // 
+            this.ButtonLink.Location = new System.Drawing.Point(938, 329);
+            this.ButtonLink.Name = "ButtonLink";
+            this.ButtonLink.Size = new System.Drawing.Size(122, 23);
+            this.ButtonLink.TabIndex = 10;
+            this.ButtonLink.Text = "Указать ссылку";
+            this.ButtonLink.UseVisualStyleBackColor = true;
+            this.ButtonLink.Click += new System.EventHandler(this.ButtonLink_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(938, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 39);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Сохранить конфиг";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(935, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Переходы";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1076, 424);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ButtonLink);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDel);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.OpenButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonDel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button ButtonLink;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using LitJson;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public static Point[] mouse = new Point[10];
+        Color color = Color.Red;
+        List<Point> UnscaledPoint = new List<Point>(20);
+        public static string CurrentFolder, ConfigPath, cleanimg;
+        public static List<Panorama> Panoramas = new List<Panorama>();
+        public static Panorama SelectedPanorama;
+        public static Buttons SelectedButton;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            //mouseList.AddRange(Enumerable.Repeat((Point)0, 20));
+            textBox1.Enabled = false;
+            ButtonLink.Enabled = false;
+            CurrentFolder = @"C:\Users\p.m.kikin\Desktop\DEVELOP\SGUGIT\Assets\Materials";
+        }
+
+        public void button1_Click(object 
+            sender, EventArgs e) //OpenFolder
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                fbd.SelectedPath = CurrentFolder;
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    Panoramas = new List<Panorama>();
+                    Panoramas.Clear();
+                    CurrentFolder = fbd.SelectedPath;
+                    comboBox1.Items.Clear();
+                    ConfigPath = System.IO.Path.Combine(CurrentFolder, "pano.cfg");
+                    string ExistingConfig;
+                    ExistingConfig = File.Exists(ConfigPath) ? File.ReadAllText(ConfigPath) : ""; //Read config file if Exists
+                    if (ExistingConfig != "") { Panoramas = JsonMapper.ToObject<List<Panorama>>(ExistingConfig); } //Config Data to Object List if not empty
+                    bool isNotEmpty = Panoramas.Any(); //is Panorama list not empty?
+
+                    //MessageBox.Show(ConfigData[3].Name.ToString());
+                    //MessageBox.Show(ConfigData.Exists(x => x.Name == "1.1").ToString());
+
+                    //-----Fill The Objetc List-------->>>>>>>>>>>>>>>
+                    foreach (string fileName in Directory.GetFiles(CurrentFolder, "*.jpg"))
+                    {
+                        if (isNotEmpty)
+                        {
+                            if (!Panoramas.Exists(x => x.Name == Path.GetFileNameWithoutExtension(fileName)))
+                            {
+                                Panoramas.Add(new Panorama
+                                {
+                                    Name = Path.GetFileNameWithoutExtension(fileName),
+                                    Buttons = new List<Buttons> { }
+                                });
+                            }
+                        }
+                        else
+                        {
+                            Panoramas.Add(new Panorama
+                            {
+                                Name = Path.GetFileNameWithoutExtension(fileName),
+                                Buttons = new List<Buttons> { }
+                            });
+                        }
+                    }
+
+                    for (int i = 0; i < Panoramas.Count; i++)
+                    {
+                        if (!Array.Exists(Directory.GetFiles(CurrentFolder, "*.jpg"), element => element.Contains(Panoramas[i].Name)))
+                        {
+                            Panoramas.RemoveAll(x => x.Name == Panoramas[i].Name);
+                            i = i - 1;
+                        }
+                    }
+                    //-----Fill The Objetc List--------<<<<<<<<<<<<<<<
+
+                    //-----Fill The Combobox and Listbox-------->>>>>>>>>>>>>>>
+                    foreach (Panorama pano in Panoramas)
+                    {
+                        UpdateComboBox(pano.Name);
+                    }
+                    //-----Fill The Combobox and Listbox--------<<<<<<<<<<<<<<<
+                    ConfigWriter.ConfigWrite(Panoramas);
+                }
+            }
+        }
+
+        private void UpdateComboBox(string PanoName) //Fill the Combobox
+        {
+            comboBox1.Items.Add(PanoName);
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e) //Add
+        {
+            SelectedPanorama.Buttons.Add(new Buttons { Link = null, PositionX = 0, PositionY = 0});
+            UpdateListBox(listBox1.Items.Count);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void buttonDel_Click(object sender, EventArgs e) // Delete
+        {
+            for (int i = listBox1.SelectedIndices.Count - 1; i >= 0; i--)
+            {
+                SelectedPanorama.Buttons.RemoveAt(listBox1.SelectedIndex);
+                UpdateListBox(listBox1.Items.Count-2);
+                pictureBox1.Load(cleanimg);
+                pictureBox1.Refresh();
+                //Repaint(true);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) //Picture Choose
+        {
+            SelectedPanorama = Panoramas.Find(item => item.Name == comboBox1.SelectedItem.ToString());
+            pictureBox1.Load(CurrentFolder+"/"+ SelectedPanorama.Name.ToString()+".jpg");
+            cleanimg = pictureBox1.ImageLocation;
+            listBox1.Items.Clear();
+            UpdateListBox(0);
+        }
+
+        private void UpdateListBox(int CurrentIndex) //Fill listbox with buttons
+        {
+            if (SelectedPanorama.Buttons != null & SelectedPanorama.Buttons.Any())
+            {
+                listBox1.Items.Clear();
+                foreach (Buttons button in SelectedPanorama.Buttons)
+                {
+                    listBox1.Items.Add("X: " + button.PositionX + " Y: " + button.PositionY);
+                }
+                listBox1.SelectedIndex = CurrentIndex;
+            }
+            else listBox1.Items.Clear();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e) //ListBox
+        {
+            UpdateTextBox();
+            Repaint(false);
+        }
+
+        private void UpdateTextBox() //Fill listbox with buttons
+        {
+            if (listBox1.Items.Count >= 1 & listBox1.SelectedIndex >= 0)
+            {
+                textBox1.Text = SelectedPanorama.Buttons[listBox1.SelectedIndex].Link;
+                textBox1.Enabled = true;
+                ButtonLink.Enabled = true;
+            }
+            else
+            {
+                textBox1.Enabled = false;
+                ButtonLink.Enabled = false;
+            }
+        }
+
+        private void ButtonLink_Click(object sender, EventArgs e) //Link choose
+        {
+            GetButtonLink();
+        }
+        private void GetButtonLink() //Link choose
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "JPEG files (*.jpg)|*.jpg|All files (*.*)|*.*";
+            dialog.InitialDirectory = CurrentFolder;
+            dialog.Title = "Выберите изображение на которое ссылается кнопка";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                if (listBox1.Items.Count >= 1 & listBox1.SelectedIndex >= 0)
+                {
+                    System.IO.FileInfo fInfo = new System.IO.FileInfo(dialog.FileName);
+                    SelectedPanorama.Buttons[listBox1.SelectedIndex].Link = Path.GetFileNameWithoutExtension(fInfo.Name.ToString());
+                    UpdateTextBox();
+                }
+            }
+        }
+        private void pictureBox1_Click(object sender, EventArgs e) //Draw points
+        {
+            if (listBox1.Items.Count >= 1 & listBox1.SelectedIndex >= 0)
+            {
+                //Point p = pictureBox1.PointToClient(Cursor.Position);
+                Point p = getTrueCoord();
+                SelectedPanorama.Buttons[listBox1.SelectedIndex].PositionX = p.X;
+                SelectedPanorama.Buttons[listBox1.SelectedIndex].PositionY = p.Y;
+                UpdateListBox(listBox1.SelectedIndex);
+                Repaint(true);
+                if (textBox1.Text == "") GetButtonLink();
+            }
+        }
+        private void Repaint(bool IfDelete) //Rpaint points
+        {
+            if (IfDelete) { pictureBox1.Load(cleanimg); pictureBox1.Refresh(); }
+            Point truepoint = new Point();
+            Color color1 = Color.Green;
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            Pen pen = new Pen(color, 4 / Scale());
+            Pen pen1 = new Pen(color1, 4 / Scale());
+
+            foreach (Buttons button in SelectedPanorama.Buttons)
+            {
+                truepoint.X = button.PositionX;
+                truepoint.Y = button.PositionY;
+                g.DrawRectangle(pen, truepoint.X, truepoint.Y, 4 / Scale(), 4 / Scale());
+            }
+            truepoint.X = SelectedPanorama.Buttons[listBox1.SelectedIndex].PositionX;
+            truepoint.Y = SelectedPanorama.Buttons[listBox1.SelectedIndex].PositionY;
+            g.DrawRectangle(pen1, truepoint.X, truepoint.Y, 4 / Scale(), 4 / Scale());
+            pictureBox1.Image = pictureBox1.Image;
+            pen.Dispose();
+            pen1.Dispose();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            foreach (Panorama pano in Panoramas)
+            {
+                System.Drawing.Image img = System.Drawing.Image.FromFile(CurrentFolder + @"\" + pano.Name+".jpg");
+                pano.Height = img.Height;
+                pano.Width = img.Width;
+                img.Dispose();
+            }
+            ConfigWriter.ConfigWrite(Panoramas);
+        }
+
+        private Point getTrueCoord()
+        {
+            Point p = pictureBox1.PointToClient(Cursor.Position);
+            Point unscaled_p = new Point();
+            int w_i = pictureBox1.Image.Width;
+            int h_i = pictureBox1.Image.Height;
+            int w_c = pictureBox1.Width;
+            int h_c = pictureBox1.Height;
+            float imageRatio = w_i / (float)h_i; // image W:H ratio
+            float containerRatio = w_c / (float)h_c; // container W:H ratio
+            if (imageRatio >= containerRatio)
+            {
+                // horizontal image
+                float scaleFactor = w_c / (float)w_i;
+                float scaledHeight = h_i * scaleFactor;
+                // calculate gap between top of container and top of image
+                float filler = Math.Abs(h_c - scaledHeight) / 2;
+                unscaled_p.X = (int)(p.X / scaleFactor);
+                unscaled_p.Y = (int)((p.Y - filler) / scaleFactor);
+            }
+            else
+            {
+                // vertical image
+                float scaleFactor = h_c / (float)h_i;
+                float scaledWidth = w_i * scaleFactor;
+                float filler = Math.Abs(w_c - scaledWidth) / 2;
+                unscaled_p.X = (int)((p.X - filler) / scaleFactor);
+                unscaled_p.Y = (int)(p.Y / scaleFactor);
+            }
+            return unscaled_p;
+        }
+        private float Scale()
+        {
+            float scaleFactor;
+            int w_i = pictureBox1.Image.Width;
+            int h_i = pictureBox1.Image.Height;
+            int w_c = pictureBox1.Width;
+            int h_c = pictureBox1.Height;
+            float imageRatio = w_i / (float)h_i;
+            float containerRatio = w_c / (float)h_c;
+            if (imageRatio >= containerRatio){scaleFactor = w_c / (float)w_i;}
+            else{scaleFactor = h_c / (float)h_i;}
+            return scaleFactor;
+        }
+    }
+
+    public class Buttons
+    {
+        public string Link { get; set; }
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+    }
+
+    public class Panorama
+    {
+        public string Name { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public List<Buttons> Buttons { get; set; }
+        //public string[] Toys { get; set; }
+    }
+
+    public class ConfigWriter
+    {
+        public static void ConfigWrite(List<Panorama> pano)
+        {
+            JsonWriter writer = new JsonWriter();
+            writer.PrettyPrint = true;
+            writer.IndentValue = 2;
+            JsonMapper.ToJson(pano, writer);
+            Form1.ConfigPath = System.IO.Path.Combine(Form1.CurrentFolder, "pano.cfg");
+            FileStream fs = new FileStream(Form1.ConfigPath, FileMode.Create, FileAccess.ReadWrite);
+            StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+            sw.WriteLine(writer);
+            sw.Dispose();
+            fs.Dispose();
+        }
+    }
+
+}
+
